04ccef0acbf8c7e6257923fc8824de17e3a6d278
JoelWhiting <joelwhitingdeangelow@gmail.com>
2024-12-08 03:34:49 -0500

    Added Javadoc comments to UtilityFunctions

--- src/UtilityFunctions.java
+++ src/UtilityFunctions.java
@@ -1,8 +1,17 @@
 import java.util.List;
 import java.util.ArrayList;
 
+/**
+ * UtilityFunctions provides common utility methods for working with numbers and strings.
+ */
 public class UtilityFunctions {
 
+    /**
+     * Returns a list of all even numbers in the input list.
+     *
+     * @param numbers List of integers to filter.
+     * @return A list containing only even numbers.
+     */
     public static List<Integer> getEvenNumbers(List<Integer> numbers) {
         List<Integer> evens = new ArrayList<>();
         for (int num : numbers) {
@@ -13,10 +22,22 @@ public class UtilityFunctions {
         return evens;
     }
 
+    /**
+     * Converts all characters in a string to uppercase.
+     *
+     * @param input The input string.
+     * @return A string with all uppercase characters.
+     */
     public static String toUpperCase(String input) {
         return input.toUpperCase();
     }
 
+    /**
+     * Returns the longest string from a list of strings.
+     *
+     * @param strings List of strings to search.
+     * @return The longest string in the list.
+     */
     public static String getLongestString(List<String> strings) {
         String longest = "";
         for (String str : strings) {
