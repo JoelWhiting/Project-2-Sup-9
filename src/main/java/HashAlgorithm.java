6b89ecbeeb10da160d6946de025f749aaa199a16
JoelWhiting <joelwhitingdeangelow@gmail.com>
2024-12-09 01:34:38 -0500

    Add Javadoc documentation to hashing functionality

--- src/main/java/HashAlgorithm.java
+++ src/main/java/HashAlgorithm.java
@@ -1,6 +1,12 @@
+/**
+ * Enumeration of supported hash algorithms.
+ */
 public enum HashAlgorithm {
+    /** MD5 hashing algorithm */
     MD5("MD5"),
+    /** SHA-1 hashing algorithm */
     SHA1("SHA-1"),
+    /** SHA-256 hashing algorithm */
     SHA256("SHA-256");
 
     private final String algorithm;
@@ -9,6 +15,10 @@ public enum HashAlgorithm {
         this.algorithm = algorithm;
     }
 
+    /**
+     * Gets the algorithm name.
+     * @return The name of the algorithm.
+     */
     public String getAlgorithm() {
         return algorithm;
     }
--- src/main/java/StringHasher.java
+++ src/main/java/StringHasher.java
@@ -1,19 +1,45 @@
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
+/**
+ * Utility class for hashing and verifying strings.
+ */
 public class StringHasher {
 
+    /**
+     * Hashes a given input string using the specified hashing algorithm.
+     * @param input The input string to hash.
+     * @param algorithm The hashing algorithm to use.
+     * @return The hashed string in hexadecimal format.
+     * @throws NoSuchAlgorithmException If the algorithm is not supported.
+     */
     public static String hashString(String input, HashAlgorithm algorithm) throws NoSuchAlgorithmException {
         MessageDigest digest = MessageDigest.getInstance(algorithm.getAlgorithm());
         byte[] hashBytes = digest.digest(input.getBytes());
         return bytesToHex(hashBytes);
     }
 
+    /**
+     * Verifies if a given input string matches the expected hash.
+     * @param input The input string to verify.
+     * @param expectedHash The expected hash value.
+     * @param algorithm The hashing algorithm used.
+     * @return True if the input matches the hash, otherwise false.
+     * @throws NoSuchAlgorithmException If the algorithm is not supported.
+     */
     public static boolean verifyHash(String input, String expectedHash, HashAlgorithm algorithm) throws NoSuchAlgorithmException {
         String computedHash = hashString(input, algorithm);
         return computedHash.equalsIgnoreCase(expectedHash);
     }
 
+    /**
+     * Generates a salted hash of the input string.
+     * @param input The input string to hash.
+     * @param salt The salt value to add to the input.
+     * @param algorithm The hashing algorithm to use.
+     * @return The salted hash in hexadecimal format.
+     * @throws NoSuchAlgorithmException If the algorithm is not supported.
+     */
     public static String generateSaltedHash(String input, String salt, HashAlgorithm algorithm) throws NoSuchAlgorithmException {
         String combined = input + salt;
         return hashString(combined, algorithm);
